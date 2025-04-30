66bc484832b85b4145b1fa54eaeaf6d0bb5049f7
JulianPaz2017 <julianpaz2017@gmail.com>
2025-04-30 10:46:02 -0300

    Ejercicio 1,2 y 3

--- TP.hs
+++ TP.hs
@@ -1,5 +1,7 @@
-import qualified Data.Map as Map
 import Heap
+import qualified Data.Map as Map
+
+
 
 -- Definimos el tipo de dato que nos permite representar árboles de Huffman
 
@@ -60,10 +62,42 @@ build_FreqMap str = build_FreqMap_aux Map.empty str
 -}
 
 map_to_HTree_list :: [(Char,Int)] -> [HTree]
-map_to_HTree_list (x:xs) E ->
+map_to_HTree_list [] tree         = tree
+map_to_HTree_list ((c,w):xs) tree = [(Leaf c w)] ++ tree
 
-trans :: FreqMap -> (Heap HTree, HTree)
-trans m = list_to_heap 
+trans :: [HTree] -> Heap HTree -> Heap HTree
+trans [] heap     = heap
+trans (x:xs) heap = trans xs (merge (N 1 x E E) heap)
+
+suma_pesos :: HTree -> HTree -> Int
+suma_pesos (Leaf _ w1) (Leaf _ w2)     = w1+w2
+suma_pesos (Leaf _ w1) (Node _ _ w2)   = w1+w2
+suma_pesos (Node _ _ w1) (Leaf _ w2)   = w1+w2
+suma_pesos (Node _ _ w1) (Node _ _ w2) = w1+w2
+
+merge_HTree :: HTree -> HTree -> HTree
+merge_HTree ht1 ht2 = (Node ht1 ht2 (suma_pesos ht1 ht2))
+
+build_HTree_aux :: Heap HTree -> HTree -> HTree
+build_HTree_aux heap min_htree
+    | (isEmpty heap) = min_htree
+    | otherwise      = let
+                          min_el    = findmin heap
+                          rest_heap = deleteMin heap
+                          new_htree = merge min_htree min_el
+                          new_heap  = insert rest_heap new_htree
+                       in
+                        build_FreqMap_aux (deleteMin new_heap) (findMin new_heap)
 
 build_HTree :: FreqMap -> HTree
-build_HTree m = if (m == Map.empty) then undefined else let heap = (trans m) in build_HTree_aux (findMin heap) (deleteMin heap)
+build_HTree m  
+  | (m == Map.empty) = undefined 
+  | otherwise = let 
+                  heap = trans (Map.fromList m) E 
+                in 
+                  build_HTree_aux (deleteMin heap) (findMin heap)
+
+
+
+
+
