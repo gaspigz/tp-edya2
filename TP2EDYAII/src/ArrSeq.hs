1a162128b3e631a384189f16952b9bc7236d36a7
JulianPaz2017 <julianpaz2017@gmail.com>
2025-06-25 17:56:27 -0300

    cambios scan

--- TP2EDYAII/src/ArrSeq.hs
+++ TP2EDYAII/src/ArrSeq.hs
@@ -7,63 +7,89 @@ import Par ((|||))
 
 lg n | n <= 1 = 0
      | otherwise = 1 + lg (n `div` 2)
+
 reduceSAux :: (a -> a -> a) -> A.Arr a -> a
-reduceSAux f xs =  case (lengthS xs) of 
-    1 -> nthS xs 0 
-    n -> let 
-            maxPotDos = 2 ^ lg (n-1)
-            (ys, zs) = reduceSAux f (takeS xs maxPotDos) ||| reduceSAux f (dropS xs maxPotDos)  
-            in f ys zs
+reduceSAux f arr =  case (lengthS arr) of 
+                            1 -> nthS arr 0 
+                            n -> let 
+                                   maxPotDos = 2 ^ lg (n-1)
+                                   (arr1, arr2) = (reduceSAux f (takeS arr maxPotDos)) ||| 
+                                                  (reduceSAux f (dropS arr maxPotDos))
+                                 in
+                                   f arr1 arr2
+
+scanAux1 :: (a -> a -> a) -> A.Arr a -> Int -> a
+scanAux1 f arr i = f (nthS arr (2*i)) (nthS arr ((2*i)+1))
+
+scanAux2 :: (a -> a -> a) -> A.Arr a -> A.Arr a -> Int -> a
+scanAux2 f arr arr' i = if 
+                         (even i)
+                        then
+                         (nthS arr (div i 2))
+                        else
+                         f (nthS arr (div i 2)) (nthS arr' (i-1))
+
 
 instance Seq A.Arr where
-    emptyS     = A.empty
+    emptyS = A.empty
+
     singletonS x = A.fromList [x] 
-    lengthS      =  A.length
-    nthS xs i  = xs ! i 
-    tabulateS  =  A.tabulate
-    fromList  = A.fromList
-    mapS   f xs      = tabulateS (\i -> f (nthS xs i)) (lengthS xs)
-    appendS xs ys = let n = lengthS xs
-                        m = lengthS ys
-                    in tabulateS (\ i -> if i < n then nthS xs i else nthS ys (i - n)) (n + m)  
-    takeS xs i     = A.subArray 0 i xs
-    dropS xs i     = A.subArray i (lengthS xs - i) xs
-    filterS   p xs    = let
-                             n = lengthS xs
-                        in fromList [x | i <- [0..n-1], let x = xs ! i, p x]
+
+    lengthS arr =  A.length arr
+
+    nthS arr i  = arr ! i 
+
+    tabulateS f n =  A.tabulate f n
+
+    fromList xs = A.fromList xs
+
+    mapS f arr = tabulateS (\i -> f (nthS arr i)) (lengthS arr)
+
+    appendS arr1 arr2 = let 
+                          n = lengthS arr1
+                          m = lengthS arr2
+                        in 
+                          tabulateS (\ i -> if i < n then nthS arr1 i else nthS arr2 (i - n)) (n + m)  
+
+    takeS arr i     = A.subArray 0 i arr
+
+    dropS arr i     = A.subArray i (lengthS arr - i) arr
+
+    filterS p arr = let
+                     n = lengthS arr
+                   in 
+                     fromList [x | i <- [0..n-1], let x = nthS arr i, p x]
+
+    showtS arr = case (lengthS arr) of
+                            0 -> EMPTY
+                            1 -> ELT (nthS arr 0) 
+                            n -> let 
+                                   (l,r) = (takeS arr ( n `div` 2)) ||| (dropS arr ( n `div` 2))
+                                 in NODE l r 
 
 
-    showtS xs = case (lengthS xs) of
-            0 -> EMPTY
-            1 -> ELT (nthS xs 0) 
-            n -> let 
-                    (l,r) = takeS xs ( n `div` 2) ||| dropS xs ( n `div` 2)
-                 in NODE l r 
+    showlS arr = case (lengthS arr) of
+                            0 -> NIL
+                            n -> let 
+                                  (l,r) = (nthS arr 0) ||| (dropS arr 1)
+                                 in CONS l r 
 
 
-    showlS xs = case (lengthS xs) of
-             0 -> NIL
-             n -> let 
-                    (l',r) = takeS xs 1 ||| dropS xs 1
-                    l = nthS l' 0
-                 in CONS l r 
+    joinS arrr = A.flatten arrr
 
-
-    joinS = A.flatten 
-
-    reduceS f b xs = case (lengthS xs) of 
-            0 -> b
-            n -> f b (reduceSAux f xs) 
+    reduceS f b arr = case (lengthS arr) of 
+                            0 -> b
+                            n -> f b (reduceSAux f arr) 
 
     
 
-    scanS f b xs = case (lengthS xs) of 
-        0 -> (A.empty, b)
-        1 -> (singletonS b, f b (nthS xs 0)) 
-        n -> let  
-                medio = div n 2
-                contraccion = tabulateS (\ i -> f (nthS xs (2*i)) (nthS xs ((2*i)+1)) ) medio 
-                (xs', end) = scanS f b contraccion
-                expansion = tabulateS (\i -> if even i then (nthS xs' (div i 2)) else (f (nthS xs' (div i 2)) (nthS contraccion (i-1)))) n
-             in
-               (expansion, end)
+    scanS f b arr = case (lengthS arr) of 
+                            0 -> (A.empty, b)
+                            1 -> (singletonS b, f b (nthS arr 0)) 
+                            n -> let  
+                                   medio = div n 2
+                                   contraccion = tabulateS (scanAux1 f arr) medio 
+                                   (arr', end) = scanS f b contraccion
+                                   expansion = tabulateS (scanAux2 f arr' contraccion) n
+                                 in
+                                   (expansion, end)
--- TP2EDYAII/src/ListSeq.hs
+++ TP2EDYAII/src/ListSeq.hs
@@ -6,40 +6,77 @@ import Par ((|||))
 lg n | n <= 1 = 0
      | otherwise = 1 + lg (n `div` 2)
 
+scanAux1 :: (a -> a -> a) -> [a] -> Int -> Int -> a
+scanAux1 f s i n = if 
+                    ((2*i) >= n-1)
+                  then 
+                    nthS s (2*i)
+                  else
+                    f (nthS s (2*i)) (nthS s ((2*i)+1))
+
+scanAux2 :: (a -> a -> a) -> [a] -> [a] -> Int -> a
+scanAux2 f s s' i = if 
+                     (even i)
+                    then
+                     (nthS s (div i 2))
+                    else
+                     f (nthS s (div i 2)) (nthS s' (i-1))
+
+
 instance Seq [] where
-    emptyS     = []
+    emptyS = []
+
     singletonS x = [x]
-    lengthS    = length
+
+    lengthS xs = length xs
+
     nthS xs i  = xs !! i
+
+    -- Versión 2
+    -- tabulateS f n = [f x | x <- [0..n-1]]
+
     tabulateS f n = map f [0..n-1]
-    mapS          = map
-    filterS       = filter
-    appendS       = (++) 
-    takeS xs i     = take i xs
-    dropS xs i     = drop i xs
-    showtS [] = EMPTY
+
+    mapS f xs = map f xs
+
+    filterS p xs = filter p xs
+
+    appendS xs ys = (xs ++ ys)
+
+    takeS xs i = take i xs
+
+    dropS xs i = drop i xs
+
+    showtS []  = EMPTY
     showtS [x] = ELT x
-    showtS    xs  = let mid = length xs `div` 2 
-                        (l,r) = takeS xs mid ||| dropS xs mid
-                    in NODE l r     
-    showlS [] = NIL
+    showtS xs  = let 
+                   mid   = length xs `div` 2 
+                   (l,r) = takeS xs mid ||| dropS xs mid
+                 in NODE l r     
+
+    showlS []     = NIL
     showlS (x:xs) = CONS x xs
-    joinS = concat --  concat [[1, 2, 3], [4, 5], [6], []] -> [1,2,3,4,5,6]
-    reduceS f b [] = b
+
+    joinS xss = concat xss
+
+    reduceS f b []  = b
     reduceS f b [x] = f b x
-    reduceS f b s = let 
-                            n = lengthS s
-                            maxPotDos = 2 ^ lg (n-1) -- lg: Returns floor of log base 2. (Hoogle)
-                            (l,r) = reduceS f b (takeS s maxPotDos) ||| reduceS f b (dropS s maxPotDos)
-                    in f l r
-    scanS f b [] = ([], b)
+    reduceS f b s   = let 
+                        n         = lengthS s
+                        maxPotDos = 2 ^ lg (n-1)
+                        (l,r)     = (reduceS f b (takeS s maxPotDos)) ||| (reduceS f b (dropS s maxPotDos))
+                      in f l r
+
+
+    scanS f b []   = ([], b)
     scanS f b [x]  = ([b], f b x)
-    scanS f b s = let
-                    n = lengthS s
-                    medio = div n 2
-                    contraccion = tabulateS (\ i -> f (nthS s (2*i)) (nthS s ((2*i)+1)))  medio 
-                    (s', end) = scanS f b contraccion
-                    expansion = tabulateS (\i -> if even i then (nthS s' (div i 2)) else (f (nthS s' (div i 2)) (nthS contraccion (i-1)))) n
-                  in
-                    (expansion, end)
+    scanS f b s    = let
+                       n           = lengthS s
+                       medio       = div n 2
+                       contraccion = tabulateS (scanAux1 f s n) (medio+1)
+                       (s', end)   = scanS f b contraccion
+                       expansion   = tabulateS (scanAux2 f s' contraccion) n
+                     in
+                       (expansion, end)
+
     fromList xs = xs
